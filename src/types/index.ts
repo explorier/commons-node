f43eba373949c9bad349c0b3467caa1739e382b9
explorier <explorier@users.noreply.github.com>
2026-01-22 23:42:27 -0600

    create type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface Channel {
+  id: string;
+  name: string;
+  stream_url: string;
+  description?: string;
+}
+
+export interface Station {
+  id: number;
+  call_sign: string;
+  name: string;
+  frequency: string;
+  location: string;
+  description: string | null;
+  stream_url: string;
+  website: string | null;
+  lat: number;
+  lng: number;
+  channels: Channel[] | null;
+  disable_now_playing: boolean;
+  created_at: Date;
+}
