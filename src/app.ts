421475032e10fd2e7edd57f5955c569b8ed11ed8
explorier <explorier@users.noreply.github.com>
2026-04-30 23:54:18 -0500

    use cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
 import stationRoutes from './routes/stationRoutes';
+import cors from 'cors';
 
 const app = express();
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') ?? [];
+
+app.use(cors({ origin: allowedOrigins }));
 
 app.use(express.json());
 
